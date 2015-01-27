d64fe6726c10cf3673b7c01c3948dddf5c724170
Eric Fenderbosch <eric.fenderbosch@segmint.com>
2015-01-27 09:19:32 -0500

    fixed request mappings

--- src/main/java/com/example/TestController.java
+++ src/main/java/com/example/TestController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,13 +17,14 @@ public class TestController {
 	@Autowired
 	private JmsTemplate jmsTemplate;
 
-	@RequestMapping
+	@RequestMapping(value = "/test", method = RequestMethod.POST)
 	public ResponseEntity<?> create(@RequestBody TestEntity testEntity) {
 		TestEntity created = repo.save(testEntity);
 		jmsTemplate.convertAndSend("testqueue", created);
 		return new ResponseEntity(created, HttpStatus.OK);
 	}
 
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
 	public ResponseEntity<?> read(Long id) {
 		return new ResponseEntity(repo.findOne(id), HttpStatus.OK);
 	}
